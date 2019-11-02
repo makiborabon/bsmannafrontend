ce04c8d3acb8078fd08d75e068f9f346182b79b9
Maki <maki.borabon@gmail.com>
2019-11-02 09:27:55 +0800

    Update services section

--- resources/js/components/services.vue
+++ resources/js/components/services.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="p-50">
         <el-row :gutter="10">
-            <el-col :xs="24" :sm="1" :md="1" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :xs="24" :sm="10" :md="6" :lg="6" :xl="1">
+            <el-col :xs="24" :sm="1" :md="1" :lg="3" :xl="3"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :xs="24" :sm="10" :md="6" :lg="6" :xl="6">
                 <el-image 
                     :src="home"
                     >
                 </el-image>
             </el-col>
-            <el-col :xs="24" :sm="10" :md="6" :lg="6" :xl="11">
+            <el-col :xs="24" :sm="10" :md="6" :lg="6" :xl="6">
                 <el-image 
                     :src="laundry"
                     >
                 </el-image>
             </el-col>
-            <el-col :xs="24" :sm="10" :md="8" :lg="6" :xl="11" clear>
+            <el-col :xs="24" :sm="10" :md="8" :lg="6" :xl="6" clear>
                 <el-image 
                     class="hotel-max-h"
                     :src="hotel"
@@ -23,8 +23,8 @@
             </el-col>
         </el-row>
         <el-row :gutter="10" >
-            <el-col :xs="24" :sm="1" :md="1" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :xs="24" :sm="10" :md="12" :lg="12" :xl="1" class="mt-210">
+            <el-col :xs="24" :sm="1" :md="1" :lg="3" :xl="3"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :xs="24" :sm="10" :md="12" :lg="12" :xl="12" class="mt-210">
                 <el-image
                     :src="restaurant"
                     >
