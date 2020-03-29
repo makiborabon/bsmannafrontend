238f42d5cdf4724ad2c74f59a8160c721ae49d54
Maki <maki.borabon@gmail.com>
2020-03-29 19:08:09 +0800

    Major Update blade template

---
+++ config/google.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "installed" => [
+        "client_id" => "306410721560-egr0k48b0vv89unplotd8i6787mjab5o.apps.googleusercontent.com",
+        "project_id" => "quickstart-1583948239924",
+        "auth_uri" => "https://accounts.google.com/o/oauth2/auth",
+        "token_uri" => "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url" => "https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret" => "IZlNC9kjyodepCPZu1da0dX0",
+        "redirect_uris"=> ["urn:ietf:wg:oauth:2.0:oob","http://localhost"]
+    ],
+];
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('default');
 });
+
+
+// Route::get('/google', function () {
+//     return view('test');
+// });
+
+
+// Route::get('/google', 'QuickStartController@loadCLient');
